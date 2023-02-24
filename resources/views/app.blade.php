5fd1b950623607ea7720b38738da1ebf6396ebc4
Derfing <derendaevkosta45@gmail.com>
2023-02-24 19:02:19 +0300

    added first page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.base')
+
+@section('main')
+    <section id="main-content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-sm-12 text-center"><h2>Каталог игр<hr></h2></div>
+            </div>
+
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-4 text-center">
+                        <img class="embed-responsive h-100 w-100" src="https://cdn-icons-png.flaticon.com/512/250/250767.png">
+                    </div>
+                    <div class="col-sm-8">Крестики-нолики — логическая игра между двумя противниками на квадратном поле 3 на 3 клетки или бо́льшего размера (вплоть до «бесконечного поля»). Один из игроков играет «крестиками», второй — «ноликами». В традиционной китайской игре Гомоку используются чёрные и белые камни.</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12"><a href="#" type="button" class="btn-lg btn-success text-center w-100 h-100">Играть</a></div>
+                </div>
+            </div>
+            <hr>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-4 text-center">
+                        <img class="embed-responsive h-100 w-100" src="https://u7.uidownload.com/vector/762/202/vector-draughts-board-vector-eps-ai.jpg">
+                    </div>
+                    <div class="col-sm-8">Шашки — логическая настольная игра для двух игроков, заключающаяся в передвижении определённым образом фишек-шашек по клеткам шашечной доски. Во время партии каждому игроку принадлежат шашки одного цвета: чёрного или белого (иногда других цветов, один из которых считается тёмным, а другой — светлым). Цель игры — взять все шашки соперника или лишить их возможности хода (запереть). Существует несколько вариантов шашек, различающихся правилами и размерами игрового поля.</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12"><a href="#" type="button" class="btn-lg btn-success text-center w-100 h-100">Играть</a></div>
+                </div>
+            </div>
+            <hr>
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-sm-4 text-center">
+                        <img class="embed-responsive h-100 w-100" src="https://i.pinimg.com/originals/e3/bb/12/e3bb1269580b9d73cecdd8ddcb832e25.jpg">
+                    </div>
+                    <div class="col-sm-8">Шахматы — настольная логическая игра с шахматными фигурами на 64-клеточной доске, сочетающая в себе элементы искусства (в том числе в части шахматной композиции), науки и спорта.
+
+                        В шахматы обычно играют два игрока (именуемые шахматистами) друг против друга. Также возможна игра одной группы шахматистов против другой или против одного игрока, такие партии зачастую именуются консультационными. Кроме того, существует практика сеансов одновременной игры, когда против одного сильного игрока играет несколько противников, каждый на отдельной доске.</div>
+                </div>
+                <div class="row">
+                    <div class="col-sm-12"><a href="#" type="button" class="btn-lg btn-success text-center w-100 h-100">Играть</a></div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
---
+++ resources/views/layouts/base.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+        @livewireStyles
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+        <link href="css/app.css" rel="stylesheet">
+    </head>
+    <body>
+    <section id="navigation">
+        <div class="container-fluid">
+            <div class="row bg-dark text-white text-center">
+                <div class="col-sm-4"><a herf="#">На главную</a></div>
+                <div class="col-sm-4"><a herf="#">Каталог игр</a></div>
+                <div class="col-sm-4"><a herf="#">Профиль</a></div>
+            </div>
+        </div>
+    </section>
+    @yield('main')
+
+    <section id="footer">
+        <footer class="container-fluid bg-dark text-white">
+            <div class="row">
+
+                <div class="col-sm-4 text-center">
+                    <h2>@Derfing</h2>
+                </div>
+
+                <div class="col-sm-2 text-center h4">
+                    <a href="#">Вконтакте</a>
+                </div>
+
+                <div class="col-sm-2 text-center h4">
+                    <a href="#">Телеграмм</a>
+                </div>
+
+                <div class="col-sm-2 text-center h4">
+                    <a href="#">Электронная почта</a>
+                </div>
+
+                <div class="col-sm-2 text-center h4">
+                    <a href="#">GitHub</a>
+                </div>
+
+            </div>
+        </footer>
+    </section>
+    @livewireScripts
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    </body>
+</html>

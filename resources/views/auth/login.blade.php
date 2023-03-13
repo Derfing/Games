2779bd9338573d0a9be56f707e8cef4bd6cca314
Derfing <derendaevkosta45@gmail.com>
2023-03-13 15:23:46 +0300

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,21 +3,18 @@
 @section('title', 'Login')
 
 @section('main')
-    <hr>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8 mx-auto">
                 <div class="card">
                     <div class="card-header">Вход</div>
-
                     <div class="card-body">
                         <form method="POST" action="{{ route('login') }}">
                             @csrf
-
                             <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+                                <label for="email" class="col-4 col-form-label">Email</label>
 
-                                <div class="col-md-6">
+                                <div class="col-6">
                                     <input id="email" placeholder="Введите адрес электронной почты" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
                                     @error('email')
@@ -29,9 +26,9 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">Пароль</label>
+                                <label for="password" class="col-4 col-form-label text-md-end">Пароль</label>
 
-                                <div class="col-md-6">
+                                <div class="col-6">
                                     <input id="password" placeholder="Придумайте надежный пароль" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                     @error('password')
@@ -43,7 +40,7 @@
                             </div>
 
                             <div class="row mb-3">
-                                <div class="col-md-6 offset-md-4">
+                                <div class="col-6 offset-4">
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -71,6 +68,7 @@
                     </div>
                 </div>
             </div>
+
         </div>
     </div>
     <hr>

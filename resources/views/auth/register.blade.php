ef2722e6de36aaff531453d036ec54f0a87245fe
Derfing <derendaevkosta45@gmail.com>
2023-03-13 15:27:08 +0300

    fix register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,10 +3,9 @@
 @section('title', 'Регистрация')
 
 @section('main')
-    <hr>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-8 mx-auto">
                 <div class="card">
                     <div class="card-header">Регистрация</div>
 
@@ -15,9 +14,9 @@
                             @csrf
 
                             <div class="row mb-3">
-                                <label for="name" class="col-md-4 col-form-label text-md-end">Имя</label>
+                                <label for="name" class="col-4 col-form-label text-md-end">Имя</label>
 
-                                <div class="col-md-6">
+                                <div class="col-6">
                                     <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
 
                                     @error('name')
@@ -29,9 +28,9 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+                                <label for="email" class="col-4 col-form-label text-md-end">Email</label>
 
-                                <div class="col-md-6">
+                                <div class="col-6">
                                     <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                                     @error('email')
@@ -43,9 +42,9 @@
                             </div>
 
                             <div class="row mb-3">
-                                <label for="password" class="col-md-4 col-form-label text-md-end">Пароль</label>
+                                <label for="password" class="col-4 col-form-label text-md-end">Пароль</label>
 
-                                <div class="col-md-6">
+                                <div class="col-6">
                                     <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
 
                                     @error('password')
@@ -59,13 +58,13 @@
                             <div class="row mb-3">
                                 <label for="password-confirm" class="col-md-4 col-form-label text-md-end">Подтвердите пароль</label>
 
-                                <div class="col-md-6">
+                                <div class="col-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                                 </div>
                             </div>
 
                             <div class="row mb-0">
-                                <div class="col-md-6 offset-md-4">
+                                <div class="col-6 offset-md-4">
                                     <button type="submit" class="btn btn-primary">
                                         Зарегистрироваться
                                     </button>
